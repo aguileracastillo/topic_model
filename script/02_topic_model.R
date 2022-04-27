d77e1406ad535121f114576df8cbd7f58921660d
aguileracastillo <102373301+aguileracastillo@users.noreply.github.com>
2022-04-27 18:51:59 +0200

    Script update

--- script/02_topic_model.R
+++ script/02_topic_model.R
@@ -1,13 +1,24 @@
 #############################
 #### Topic Model for DGRL v 17.5 ==> 6682 JOURNAL ARTICLES
 #############################
+## RUN PACKAGES
+library(quanteda)
+library(quanteda.textmodels)
+library(quanteda.textplots)
+library(quanteda.textstats)
+library(stm)
+library(stminsights)
+library(wordcloud)
 
 ## Topic Model of Journal Articles
 dgrl_tm <- to_corpus %>% filter(type.x == "journalArticle")
 
+## r articles by year to insert in md
 dgrl_tm %>% group_by(year.x) %>% count(sort = TRUE) %>%
   ggplot(aes(year.x, n)) +
   geom_line()+
   xlab ("Year") +
   ylab ("Number of Documents")
 
+## Most frequent publication titles
+dgrl_tm %>% count(pub_title.x) %>% arrange(desc(n))
