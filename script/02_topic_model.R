3db461305f0bff67495e261e0e95feed29e07a91
aguileracastillo <102373301+aguileracastillo@users.noreply.github.com>
2022-05-04 12:52:58 +0200

    Editing in text and script

--- script/02_topic_model.R
+++ script/02_topic_model.R
@@ -56,9 +56,71 @@ print(dgrl175_tokens)
 ## No stemming to provide a more human readable descriptor (De Battisti et al 2015)
 
 ### How to view a document in a corpus
-dgrl175_corpus[[521]]
+dgrl175_corpus[[849]]
 
-## KEYWORDS IN CONTEXT
+#### KEYWORDS-IN-CONTEXT (theories sample test)
+# NPM 28 MATCHES
+kw_npm <- kwic(dgrl175_tokens, pattern = "npm")
+head(kw_npm)
+View(kw_npm)
+# UTAUT 96 MATCHES
+kw_utaut <- kwic(dgrl175_tokens, pattern = "utaut")
+head(kw_utaut)
+View(kw_utaut)
+# THEOR* 1950 MATCHES --> CHECK CO-OCCURRENCE
+kw_theor <- kwic(dgrl175_tokens, pattern = "theor*")
+head(kw_theor)
+View(kw_theor)
+# QUALITAT* 448 MATCHES
+kw_ql <- kwic(dgrl175_tokens, pattern = "qualitat*")
+View(kw_ql)
+# QUANTITAT* 292 MATCHES
+kw_qt <- kwic(dgrl175_tokens, pattern = "quantitat*")
+head(kw_qt)
+View(kw_qt)
+# EMPIRIC* 984 MATCHES
+kw_emp <- kwic(dgrl175_tokens, pattern = "empiric*")
+head(kw_emp)
+View(kw_emp)
+# BIBLIO* 48 MATCHES
+kw_biblio <- kwic(dgrl175_tokens, pattern = "biblio*")
+View(kw_biblio)
 
+## KWIC PHRASES
+# PUBLIC VALUE* 289 MATCHES
+kw_pv <- kwic(dgrl175_tokens, pattern = phrase("public value*"))
+head(kw_pv)
+View(kw_pv)
+# NEW PUBLIC MANAGEMENT 40 MATCHES 
+kw_new_pub_mgmt <- kwic(dgrl175_tokens, pattern = phrase("new public management"))
+head(kw_new_pub_mgmt)
+
+#################
 ## Create a dictionary of words of interest
+################
+dgrl175_dictionary <- dictionary(list(verb = c("reduc*", "replac*", "elimin*", "save", "lower", "substitut*", "autom*"),
+                                      object = c("labor", "worker*", "human", "employe*", "manpow*", "job*"),
+                                      attribute = c("cost*", "expenditur*", "expense*", "hour*", "intens*", "task*", "time", "skill")))
 
+## Test tokens_lookup
+labor_saving <- tokens_lookup(dgrl175_tokens, dgrl175_dictionary, nomatch = "NULL")
+
+tokens_lookup(dgrl175_tokens, dgrl175_dictionary, nomatch = "other") %>%
+  dfm() %>%
+  dfm_weight(scheme = "prop")
+
+#### CONSTRUCT A DOCUMENT-FEATURE MATRIX (DFM) ####
+dfm_dgrl175 <- dfm(dgrl175_tokens)
+print(dfm_dgrl175)
+ndoc(dfm_dgrl175)
+nfeat(dfm_dgrl175)
+
+### TOP FEATURES IN DFM_DGRL -- Best practice remove very rare and very common
+topfeatures(dfm_dgrl175, 250)
+## Wordclouds
+set.seed(123)
+textplot_wordcloud(dfm_dgrl175, max_words = 100)
+
+### TFIDF Term Frequency-Inverse Document Frequency
+dfm_dgrl_tfidf <- dfm_tfidf(dfm_dgrl)
+print(dfm_dgrl_tfidf)
