2b72bc6f56873b78bd34ef40146c56dfefa53ea9
aguileracastillo <102373301+aguileracastillo@users.noreply.github.com>
2023-05-20 22:20:06 -0500

    Fine-tuning the Training set

--- script/03_stm_train_dgrl.R
+++ script/03_stm_train_dgrl.R
@@ -76,60 +76,59 @@ find_needle2 <- searchK(train_stm$documents,
 
 plot(find_needle2)
 
-### Potential options for K (S/M/L) => 22, 44, 53, 80
+### Potential options for K (S/M/L) => 29, 44, 65, 98
 
 #### CALCULATE STM k = 29 ####
-dgrl_stm22 <- stm(train_stm$documents, 
+dgrl_stm29 <- stm(train_stm$documents, 
                   train_stm$vocab, 
-                  K = 22,
+                  K = 29,
                   prevalence = ~ year.x,
                   max.em.its = 75,
                   data = train_stm$meta, 
                   init.type = "Spectral")
 
 ## PRINT WORDS PER TOPIC
-data.frame(t(labelTopics(dgrl_stm22, n = 10)$prob))
+data.frame(t(labelTopics(dgrl_stm29, n = 10)$prob))
 
-train22_labels <- labelTopics(dgrl_stm22, n = 10)
-train22_labels
+train29_labels <- labelTopics(dgrl_stm29, n = 10)
+train29_labels
 
 ## ESTIMATE EFFECT
-fx_22 <- estimateEffect(1:22 ~ year.x, 
-                        dgrl_stm22, 
+fx_29 <- estimateEffect(1:29 ~ year.x, 
+                        dgrl_stm29, 
                         meta = out$meta, 
                         uncertainty = "Global")
 
 ## VISUALIZE TOPIC PREVALENCE
 
-## Topic 7 Covid-19
-plot(fx_22, "year.x", method = "continuous", topics = 7,
+## Topic 4 Human Capital
+plot(fx_29, "year.x", method = "continuous", topics = 4,
      model = z, printlegend = FALSE, xaxt = "n", xlab = "Year")
 axis(1,at=c(2000,2005,2010,2015,2020),
      labels=c(2000,2005,2010,2015,2020),las=2)
 
-## Topic 11 Elections 
-plot(fx_22, "year.x", method = "continuous", topics = 11,
+## Topic 3 AI 
+plot(fx_29, "year.x", method = "continuous", topics = 3,
      model = z, printlegend = FALSE, xaxt = "n", xlab = "Year")
 axis(1,at=c(2000,2005,2010,2015,2020),
      labels=c(2000,2005,2010,2015,2020),las=2)
 
-## Topic 4 AI
-plot(fx_22, "year.x", method = "continuous", topics = 4,
+## Topic 11 e-Participation
+plot(fx_29, "year.x", method = "continuous", topics = 11,
      model = z, printlegend = FALSE, xaxt = "n", xlab = "Year")
 axis(1,at=c(2000,2005,2010,2015,2020),
      labels=c(2000,2005,2010,2015,2020),las=2)
-
 
 ## Topic Prevalence over Time ##
-par(mfrow=c(3,3))
-for (i in seq_along(sample(1:22, size = 9)))
+par(mfrow=c(3,3))  # Set up a 3x3 grid
+for (i in seq_along(sample(1:29, size = 29))) # Loop over all 29 topics
 {
-  plot(fx_22, "year.x", method = "continuous", topics = i, main = paste0(train22_labels$prob[i,1:3], collapse = ", "), printlegend = F)
+  plot(fx_29, "year.x", method = "continuous", topics = i, main = paste0(train29_labels$prob[i,1:3], collapse = ", "), printlegend = )
 }
 
 ## SHARE OF TOPICS OVER ALL CORPUS ##
 plot(
-  dgrl_stm22,
+  dgrl_stm29,
   type = "summary",
   text.cex = 0.5,
   main = "STM topic shares",
@@ -155,19 +154,19 @@ fx_44 <- estimateEffect(1:44 ~ year.x,
                        meta = out$meta, 
                        uncertainty = "Global")
 
-## Topic 10 Cybersecurity
+## Topic 10 Smart X
 plot(fx_44, "year.x", method = "continuous", topics = 10,
      model = z, printlegend = FALSE, xaxt = "n", xlab = "Year")
 axis(1,at=c(2000,2005,2010,2015,2020),
      labels=c(2000,2005,2010,2015,2020),las=2)
 
-## Topic 43 Covid-19
+## Topic 43 Web
 plot(fx_44, "year.x", method = "continuous", topics = 43,
      model = z, printlegend = FALSE, xaxt = "n", xlab = "Year")
 axis(1,at=c(2000,2005,2010,2015,2020),
      labels=c(2000,2005,2010,2015,2020),las=2)
 
-## Topic 8 Cloud
+## Topic 8 e-Services
 plot(fx_44, "year.x", method = "continuous", topics = 8,
      model = z, printlegend = FALSE, xaxt = "n", xlab = "Year")
 axis(1,at=c(2000,2005,2010,2015,2020),
@@ -176,9 +175,9 @@ axis(1,at=c(2000,2005,2010,2015,2020),
 
 ## Topic Prevalence over Time ##
 par(mfrow=c(3,3))
-for (i in seq_along(sample(1:44, size = 9)))
+for (i in seq_along(sample(1:44, size = 44)))
 {
-  plot(fx_44, "year.x", method = "continuous", topics = i, main = paste0(train44_labels$prob[i,1:3], collapse = ", "), printlegend = F)
+  plot(fx_44, "year.x", method = "continuous", topics = i, main = paste0(train44_labels$prob[i,1:3], collapse = ", "), printlegend = T)
 }
 
 ## SHARE OF TOPICS OVER ALL CORPUS ##
@@ -190,10 +189,10 @@ plot(
   xlab = "Share estimation")
 
 
-#### CALCULATE STM k = 53 ####
-dgrl_stm53 <- stm(train_stm$documents, 
+#### CALCULATE STM k = 65 ####
+dgrl_stm65 <- stm(train_stm$documents, 
                   train_stm$vocab, 
-                  K = 53,
+                  K = 65,
                   prevalence = ~ year.x,
                   max.em.its = 75,
                   data = train_stm$meta, 
@@ -201,68 +200,68 @@ dgrl_stm53 <- stm(train_stm$documents,
 
 ## SHARE OF TOPICS OVER ALL CORPUS ##
 plot(
-  dgrl_stm53,
+  dgrl_stm65,
   type = "summary",
   text.cex = 0.8,
   main = "Estimated Topic Proportions Training Set",
   xlab = "Share estimation")
 
 ## PRINT WORDS PER TOPIC
-data.frame(t(labelTopics(dgrl_stm53, n = 10)$prob))
+data.frame(t(labelTopics(dgrl_stm65, n = 10)$prob))
 
-train53_labels <- labelTopics(dgrl_stm53, n = 10)
-train53_labels
+train65_labels <- labelTopics(dgrl_stm65, n = 10)
+train65_labels
 
 ## Top 3 Topics
-top5_train53 <- labelTopics(dgrl_stm53, c(47,27,51,45,23))
-top5_train53
+top5_train65 <- labelTopics(dgrl_stm65, c(35,62,45,16,48))
+top5_train65
 
-plot(dgrl_stm53,
+plot(dgrl_stm65,
      main = "Most Prevalent Topics and Words by Score Measure",
      type="labels",
      labeltype = "score",
-     topics=c(47,27,51,45,23))
+     topics=c(35,62,45,16,48))
 
 ## Sage Labels?
-print(sageLabels(dgrl_stm53))
+print(sageLabels(dgrl_stm65))
 
-fx_53 <- estimateEffect(1:53 ~ year.x, 
-                        dgrl_stm53, 
+fx_65 <- estimateEffect(1:65 ~ year.x, 
+                        dgrl_stm65, 
                         meta = out$meta, 
                         uncertainty = "Global")
 
-## Topic 6 Mobile Phone
-plot(fx_53, "year.x", method = "continuous", topics = 6,
+## Topic 6 OGD
+plot(fx_65, "year.x", method = "continuous", topics = 6,
      model = z, printlegend = FALSE, xaxt = "n", xlab = "Year")
 axis(1,at=c(2000,2005,2010,2015,2020),
      labels=c(2000,2005,2010,2015,2020),las=2)
 
-## Topic 38 Web
-plot(fx_53, "year.x", method = "continuous", topics = 38,
+## Topic 39 Cyber
+plot(fx_65, "year.x", method = "continuous", topics = 39,
      model = z, printlegend = FALSE, xaxt = "n", xlab = "Year")
 axis(1,at=c(2000,2005,2010,2015,2020),
      labels=c(2000,2005,2010,2015,2020),las=2)
 
-## Topic 9 SM and Elections
-plot(fx_53, "year.x", method = "continuous", topics = 25,
+## Topic 57 Twitter
+plot(fx_65, "year.x", method = "continuous", topics = 57,
      model = z, printlegend = FALSE, xaxt = "n", xlab = "Year")
 axis(1,at=c(2000,2005,2010,2015,2020),
      labels=c(2000,2005,2010,2015,2020),las=2)
 
 
 ## Topic Prevalence over Time ##
-par(mfrow=c(1,1))
-for (i in seq_along(sample(1:53, size = 53)))
+par(mfrow=c(3,3))
+for (i in seq_along(sample(1:65, size = 65)))
 {
-  plot(fx_53, "year.x", method = "continuous", topics = i, main = paste0(train53_labels$prob[i,1:3], collapse = ", "), printlegend = T)
+  plot(fx_65, "year.x", method = "continuous", topics = i, main = paste0(train65_labels$prob[i,1:3], collapse = ", "), printlegend = T)
 }
 
 ## Tweak Plot ??##
 ## Topic Prevalence over Time ##
 par(mfrow=c(1,1))
-for (i in seq_along(sample(1:53, size = 53)))
+for (i in seq_along(sample(1:65, size = 65)))
 {
-  plot(fx_53, "year.x", method = "continuous", topics = c(4,8,26), main = paste0(train53_labels$prob[i,1:3], collapse = ", "), printlegend = T)
+  plot(fx_65, "year.x", method = "continuous", topics = c(4,8,26), main = paste0(train65_labels$prob[i,1:3], collapse = ", "), printlegend = T)
 }
 
 ## Topic Quality
@@ -302,7 +301,7 @@ td_theta21
 
 
 ## WORDCLOUD OF MOST PREVALENT TOPIC review usefulness
-stm::cloud(dgrl_stm53,
+stm::cloud(dgrl_stm65,
            topic = 6,
            scale = c(3.25, .95))
 
@@ -310,10 +309,10 @@ stm::cloud(dgrl_stm53,
 plot(dgrl_stm17, type = "perspectives", topics = c(6, 12)) 
 
 ## DOCUMENT TOPIC PROPORTIONS
-plot(dgrl_stm53, type = "hist", topics = sample(1:53, size = 9))
+plot(dgrl_stm65, type = "hist", topics = sample(1:53, size = 9))
 
-#### LDAvis k=53 ####
-toLDAvis(dgrl_stm53,
+#### LDAvis k=29 ####
+toLDAvis(dgrl_stm29,
          train_stm$documents,
          R = 25,
          plot.opts = list(xlab = "PC1", ylab = "PC2"),
